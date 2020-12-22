41356733f8aaa7bb5ca9e87c2bdbb877b37cf949
agatakolohe <agatakolohe@gmail.com>
2020-12-22 11:48:28 -0800

    diplay animal name and type after form submission
    Co-authored-by: David Sterry <sterry.david@gmail.com>

--- AnimalShelter/Models/Animal.cs
+++ AnimalShelter/Models/Animal.cs
@@ -1,11 +1,14 @@
 using System.Collections.Generic;
 
+
 namespace AnimalShelter.Models
 {
     public class Animal
     {
         public int AnimalId { get; set; }
+        //[Display(AnimalType = "Animal Type")]
         public string AnimalType { get; set; }
+        //[Display(AnimalName = "Animal Name")]
         public string AnimalName { get; set; }
     }
 }
